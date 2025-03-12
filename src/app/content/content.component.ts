665be45a6167bc4918a623607e04d69379d74dd1
AVANADE-CORP\jose.wesley.da.silva <jose.wesley.da.silva@avanade.com>
2025-03-11 21:40:15 -0300

    feat: logic from the login screen

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -4,13 +4,13 @@ import { Record } from '../models/record.model';
 import { CommonModule } from '@angular/common';
 import { AuthenticatorService } from '../services/authenticator.service';
 import { User } from '../models/user.model';
-
-
+import { LoginScreenComponent } from "./login-screen/login-screen.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginScreenComponent, HomeComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
@@ -18,12 +18,25 @@ export class ContentComponent implements OnInit {
   records: Record[] = [];
   data: any;
   user?: User;
+  loginValidate: boolean = false;
 
-  constructor(private supabaseService: SupabaseService, private authenticatorService: AuthenticatorService) { }
+  constructor(private supabaseService: SupabaseService, private authenticatorService: AuthenticatorService) {
+  }
+  ngOnInit(): void {
+    if (!this.user) {
+      this.getUserLocalStorage();
+    }
+  }
 
-  ngOnInit() {
-    this.selectInRecordsWithDetails_Origin();
-    this.userAuth("j.wesley", "123@321");
+  getUserLocalStorage() {
+    const userLocalStorage = localStorage.getItem('user');
+    if (userLocalStorage && userLocalStorage != 'undefined' ) {
+      this.user = JSON.parse(userLocalStorage);
+      const loginValidateLocalStorage = localStorage.getItem('loginValidate');
+      if (loginValidateLocalStorage) {
+        this.loginValidate = JSON.parse(loginValidateLocalStorage);
+      }
+    }
   }
 
   selectInRecordsWithDetails_Origin() {
@@ -35,15 +48,19 @@ export class ContentComponent implements OnInit {
     });
   }
 
-  userAuth(login: string, password: string) {
-    this.authenticatorService.userAuth(login, password).then(user => {
-      if (user) {
-        this.user = user;
-        console.log('Usuário:', this.user);
-      }
-    }).catch(error => {
-      alert('Erro ao buscar usuário: ' + error);
-    });
+  setUser($event: User) {
+    this.user = $event;
+    localStorage.setItem('user', JSON.stringify(this.user));
+  }
+  validateLogin($event: boolean) {
+    this.loginValidate = $event;
+    localStorage.setItem('loginValidate', JSON.stringify(this.loginValidate));
   }
 
+  logout() {
+    this.user = undefined;
+    this.loginValidate = false;
+    localStorage.setItem('user', JSON.stringify(this.user));
+    localStorage.setItem('loginValidate', JSON.stringify(this.loginValidate));
+  }
 }
