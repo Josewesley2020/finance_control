caa9c3dfcaf4043c6211f1389930d82ca525757f
AVANADE-CORP\jose.wesley.da.silva <jose.wesley.da.silva@avanade.com>
2025-03-24 17:38:44 -0300

    feat: Adjustment of usability, change btn of action in the grid and notification

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -10,9 +10,9 @@ import { TableRecordsIncomeService } from '../../services/table-records-income.s
 import { RecordIncome } from '../../models/record-income.model';
 import { TableGoalsService } from '../../services/table-goals.service';
 import { Goal } from '../../models/goal.model';
-import { TableDetailsOriginService } from '../../services/table-details-origin.service';
 import { ModalInsertRecordIncomeComponent } from '../modais/modal-insert-record-income/modal-insert-record-income.component';
 import { MatDialog } from '@angular/material/dialog';
+import { NotificacoesService } from '../shared/notificacoes.service';
 
 @Component({
   selector: 'app-home',
@@ -42,9 +42,9 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
+   private notificacoesService: NotificacoesService,
     private dialog: MatDialog,
     private tableRecordsService: TableRecordsService,
-    private tableIncomeSourceService: TableIncomeSourceService,
     private tableRecordsIncomeService: TableRecordsIncomeService,
     private tableGoalsService: TableGoalsService) { }
 
@@ -54,6 +54,7 @@ export class HomeComponent implements OnInit {
     this.generateDates();
     this.getRecordsIncome();
   }
+
   openModal_ModalInsertRecordIncomeComponent() {
     const { month, year } = this.parseDateString(this.selectedDate);
     const dialogRef = this.dialog.open(ModalInsertRecordIncomeComponent, {
@@ -66,34 +67,34 @@ export class HomeComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result && result.success) {
         this.getRecordsIncome();
-      } else {
-        console.log('Ação cancelada');
       }
     });
   }
+
   getRecordsIncome() {
     const userId = this.user?.id || '';
     const { month, year } = this.parseDateString(this.selectedDate);
     this.tableRecordsIncomeService.selectInRecods_income(userId).then(RecordsIncome => {
-      console.log('RecordsINCOME:', RecordsIncome);
       this.allRecordsIncomes = RecordsIncome.filter(record => record.month === month && record.year === year);
       this.valueRecordsIncome = 0;
       this.allRecordsIncomes.forEach(recordIncome => {
         this.valueRecordsIncome += parseFloat(recordIncome.value.toFixed(2));
       });
-      console.log('ValueRecordsINCOME:', this.valueRecordsIncome);
     }).catch(error => {
-      console.error('Erro ao buscar registros:', error);
+    this.notificacoesService.erro('Erro ao buscar registros de renda.');
     });
   }
+
   getRecords() {
     this.tableRecordsService.selectInRecordsWithDetails_Origin().then(records => {
       this.allRecords = records;
       this.filterRecords();
     }).catch(error => {
-      console.error('Erro ao buscar registros:', error);
+    this.notificacoesService.erro('Erro ao buscar registros.');
+    console.error('Erro ao buscar registros:', error);
     });
   }
+
   insertInRecords_Expenses() {
     const idDetailsOrigin = 41;
     const value = 100;
@@ -103,15 +104,16 @@ export class HomeComponent implements OnInit {
     const definitive_value = true;
     const payment = false;
     this.tableRecordsService.insertInInRecords_Expenses(idDetailsOrigin, value, month, year, disconts, definitive_value, payment).then(records => {
-      console.log('Adicionado com sucesso:', records);
+      this.notificacoesService.sucesso('Despesa adicionada com sucesso.');
       this.getRecords();
     }).catch(error => {
       console.error('Erro ao adicionar despesa:', error);
     });
   }
+
   updateInRecords_Expenses(id: number, value: number, discounts: number, definitive_value: boolean, payment: boolean) {
     this.tableRecordsService.updateInRecords_Expenses(id,value,discounts,definitive_value,payment).then(records => {
-      console.log('Atualizado com sucesso:', records);
+      this.notificacoesService.sucesso('Despesa atualizada com sucesso.');
       this.getRecords();
     }).catch(error => {
       console.error('Erro ao atualizar despesa:', error);
@@ -120,25 +122,29 @@ export class HomeComponent implements OnInit {
   }
   deleteInRecords_Expenses(id: number) {
     this.tableRecordsService.deleteInRecords_Expenses(id).then(records => {
-      console.log('Deletado com sucesso:', records);
+      this.notificacoesService.sucesso('Despesa deletada com sucesso.');
       this.getRecords();
     }).catch(error => {
+     this.notificacoesService.erro('Erro ao deletar despesa.');
       console.error('Erro ao deletar despesa:', error);
     });
   }
+
   informPayment(record: Record) {
     this.updateInRecords_Expenses(record.id, record.value, record.discounts, record.definitive_value, true);
   }
+
   deleteRecord(record: Record) {
-    console.log('Registro deletado:', record);
     this.deleteInRecords_Expenses(record.id);
   }
+
   editRecord(record: Record) {
     console.log('Registro editado:', record);
   }
   showInfo(record: Record) {
     console.log('Registro editado:', record);
   }
+
   generateDates() {
     const currentYear = new Date().getFullYear();
     for (let year = currentYear; year <= currentYear + 5; year++) {
@@ -147,17 +153,20 @@ export class HomeComponent implements OnInit {
       }
     }
   }
+
   parseDateString(dateString: string): { month: number, year: number } {
     const [monthStr, yearStr] = dateString.split('-');
     const month = this.months.indexOf(monthStr) + 1;
     const year = parseInt(yearStr, 10);
     return { month, year };
   }
+
   onchangeDate() {
     this.filterRecords();
     this.getRecordsIncome();
 
   }
+
   filterRecords() {
     const { month, year } = this.parseDateString(this.selectedDate);
     this.recordsFiltrados = this.allRecords.filter(record => record.month === month && record.year === year);
@@ -167,6 +176,7 @@ export class HomeComponent implements OnInit {
     this.totalLate = parseFloat(totals.totalLate.toFixed(2));
     this.totalDiscount = parseFloat(totals.totalDiscount.toFixed(2));
   }
+
   calculateTotals(records: any[]): { totalPayable: number, totalPending: number, totalLate: number, totalDiscount: number } {
     let totalPayable = 0;
     let totalPending = 0;
@@ -189,6 +199,7 @@ export class HomeComponent implements OnInit {
 
     return { totalPayable, totalPending, totalLate, totalDiscount };
   }
+
   getNextMonthAndYear(month: number, year: number) {
     let nextMonth = month + 1;
     let nextYear = year;
@@ -198,6 +209,7 @@ export class HomeComponent implements OnInit {
     }
     return { nextMonth, nextYear };
   }
+
   filterValueNextMonth(details_origin_id: number) {
     const { month, year } = this.parseDateString(this.selectedDate);
     const { nextMonth, nextYear } = this.getNextMonthAndYear(month, year);
@@ -213,6 +225,7 @@ export class HomeComponent implements OnInit {
 
     return recordFromNextMonth.value - recordFromNextMonth.discounts || 0;
   }
+
   filterValueAllMonths(details_origin_id: number) {
     const { month, year } = this.parseDateString(this.selectedDate);
     const { nextMonth, nextYear } = this.getNextMonthAndYear(month, year);
@@ -230,36 +243,44 @@ export class HomeComponent implements OnInit {
     );
     return parseFloat(value.toFixed(2));
   }
+
   truncate(text: string, limit: number): string {
     return text.length > limit ? text.substring(0, limit) + '...' : text;
   }
+
   classByStatus(record: Record): string {
     if (record.payment) return 'text-success';
     if (this.isDatePast(record.Details_Origin.due_date, record.month, record.year)) return 'text-danger';
     return 'text-warning';
   }
+
   showBtnUpdateValue(record: Record): string {
     if (record.payment) return 'text-bg-success';
     if (this.isDatePast(record.Details_Origin.due_date, record.month, record.year)) return 'text-bg-danger';
     return 'text-bg-warning';
   }
+
   isDatePast(day: number, month: number, year: number): boolean {
     const currentDate = new Date();
     const inputDate = new Date(year, month - 1, day);
 
     return inputDate < currentDate;
   }
+
   addNewGoal(): void {
     // Lógica para adicionar um novo objetivo
     console.log('Adicionar Novo Objetivo');
   }
+
   addNewExpense(): void {
     this.insertInRecords_Expenses();
   }
+
   addNewIncome(): void {
     // Lógica para adicionar uma nova renda
     console.log('Adicionar Nova Renda');
   }
+
   addReserve() {
     this.reserveAdded = true;
     this.totalReserve += 100;
@@ -267,15 +288,5 @@ export class HomeComponent implements OnInit {
       this.reserveAdded = false;
     }, 200);
   }
-  getGoals() {
-    const userId = this.user?.id || '';
-    this.tableGoalsService.selectInInGoals(userId).then(goals => {
-      this.allGoals = goals;
-      console.log('GOALS:', goals);
-      this.loadingGoals = false;
-    }).catch(error => {
-      console.error('Erro ao buscar registros:', error);
-      this.loadingGoals = false;
-    });
-  }
+
 }
